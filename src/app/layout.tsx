c392a23d853e3f8d117cdc0ce512f3f73ae20493
cit1566 <cit1566@gmail.com>
2025-12-03 18:01:40 +0900

    chore : font 기본 설정 - Pretendard 폰트 기본 폰트로 지정 - 이외의 폰트는 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,23 @@
+import "@/styles/main.css"
 import type { Metadata } from "next"
+import localFont from "next/font/local"
 import type { PropsWithChildren } from "react"
 
 export const metadata: Metadata = {
   title: "Next.js 프로젝트 템플릿",
 }
 
+const pretendard = localFont({
+  variable: "--pretendard",
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+})
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko-KR">
-      <body>
+      <body className={pretendard.className}>
         <main>{children}</main>
       </body>
     </html>
