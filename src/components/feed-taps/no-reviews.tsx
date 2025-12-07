a48ff3aa9959fc098a2f1ac58765381617bf2a41
chajiiiii <j.cha1708@gmail.com>
2025-12-07 17:04:38 +0900

    feat : NoReviews 컴포넌트 UI

---
+++ src/components/feed-taps/no-reviews.tsx
@@ -0,0 +1,9 @@
+import styles from "./no-reviews.module.css"
+
+export default function NoReviews() {
+  return (
+    <div className={styles.noReviews}>
+      <p>작성한 감상평이 없습니다.</p>
+    </div>
+  )
+}
