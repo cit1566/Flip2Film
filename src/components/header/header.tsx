030e55d7767a9e42d3d6e9be49c22ff656c3d197
cit1566 <cit1566@gmail.com>
2025-12-09 00:12:56 +0900

    feat : 해더 스크롤 숨김/보임 기능 추가

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,46 @@
+"use client"
+
 import { Search } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 import styles from "./header.module.css"
 
-export default function Header() {
+interface HeaderProps {
+  className?: string | undefined
+}
+
+export default function Header({ className }: HeaderProps) {
+  const [show, setShow] = useState(true)
+  const [lastScroll, setLastScroll] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const current = window.scrollY
+      if (current > lastScroll) {
+        setShow(false)
+      } else {
+        setShow(true)
+      }
+
+      setLastScroll(current)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [lastScroll])
+
   return (
-    <header className={styles.header}>
+    <header
+      className={`${styles.header} ${className} ${show ? styles.show : styles.hide}`}
+    >
       {/* Logo */}
       <Link href="/" className={styles.logo} aria-label="메인 페이지로 이동">
         <Image
-          src="/icon/logo.svg"
+          src="/logo/logo.svg"
           alt="Flip2Film 로고"
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           priority
         />
       </Link>
