4b8599bdf8de72a3545444588d20cb9fc92ec8e5
cit1566 <cit1566@gmail.com>
2025-12-09 17:14:30 +0900

    feat : 최 상단으로 올리기 기능 추가

--- src/components/main-body/main-body.tsx
+++ src/components/main-body/main-body.tsx
@@ -1,18 +1,48 @@
+"use client"
+
+import { ArrowBigUp } from "lucide-react"
 import Link from "next/link"
 import itemListDummy from "../main-review-item/dummy.json"
 import MainReviewItem from "../main-review-item/main-review-item"
 import styles from "./main-body.module.css"
 
-export default function MainBody() {
+interface MainBodyProps {
+  category: "Home" | "Movie" | "Book"
+}
+
+export default function MainBody({ category }: MainBodyProps) {
+  const activeLineKey = `active${category}`
+
+  function handleClickPageUp() {
+    const scrollY = window.scrollY
+    if (scrollY > 0) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+    }
+  }
+
   return (
     <div className={styles.mainBodyBox}>
       <div className={styles.navLinkBox}>
-        <Link href="/" className={styles.isActive}>
+        <Link href="/" className={`${category === "Home" && styles.isActive}`}>
           홈
         </Link>
-        <Link href="./movie">영화</Link>
-        <Link href="./books">도서</Link>
-        <div className={styles.activeLine}></div>
+        <Link
+          href="./movie"
+          className={`${category === "Movie" && styles.isActive}`}
+        >
+          영화
+        </Link>
+        <Link
+          href="./book"
+          className={`${category === "Book" && styles.isActive}`}
+        >
+          도서
+        </Link>
+        {/* 활성화 탭 밑줄 */}
+        <div className={`${styles.activeLine} ${styles[activeLineKey]}`}></div>
       </div>
       <div className={styles.contentBox}>
         {itemListDummy.map(item => {
@@ -28,6 +58,14 @@ export default function MainBody() {
           )
         })}
       </div>
+      <button
+        type="button"
+        onClick={handleClickPageUp}
+        className={styles.pageUpButton}
+        title="페이지 상단으로 이동"
+      >
+        <ArrowBigUp width={25} height={25} />
+      </button>
     </div>
   )
 }
