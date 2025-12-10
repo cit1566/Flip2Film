427f69d0202227480727411fc406d487ed11d4b2
cit1566 <cit1566@gmailc.om>
2025-12-10 16:07:27 +0900

    style : 메인페이지 캐러셀 박스 디자인 완성

--- src/components/main-body/home/home.tsx
+++ src/components/main-body/home/home.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Image from "next/image"
+import { useRef, useState } from "react"
 import styles from "./home.module.css"
 
 export default function Home() {
+  const [currentImageNum, setCurrentImageNum] = useState<number>(1)
+  const imageMarker = useRef<HTMLDivElement>(null)
+
+  const activeImagepageNum = `image_${currentImageNum}`
+
   return (
     <div className={styles.homeBox}>
       <div className={styles.carousel}>
@@ -35,6 +43,28 @@ export default function Home() {
           width={50}
           height={100}
         ></Image>
+      </div>
+
+      {/* 캐러셀 넘버 컴포넌트 */}
+      <div className={styles.imageNumberBox}>
+        <div
+          ref={imageMarker}
+          className={`${styles.currentImageMarker} ${styles[activeImagepageNum]}`}
+        ></div>
+        <div className={styles.imageNumber}>
+          {Array.from({ length: 5 }, (_, index) => {
+            return (
+              <button
+                key={index + 1}
+                type="button"
+                onClick={() => setCurrentImageNum(index + 1)}
+                className={`${currentImageNum === index + 1 && styles.activeImagePageNum}`}
+              >
+                {index + 1}
+              </button>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
