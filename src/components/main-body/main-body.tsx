ff8a30afdfdc044e156692fa90b8e6de687aa6d5
cit1566 <cit1566@gmail.com>
2025-12-11 17:38:28 +0900

    refactor : 영화 페이지, 도서 페이지 접근성 개선

--- src/components/main-body/main-body.tsx
+++ src/components/main-body/main-body.tsx
@@ -10,6 +10,10 @@ interface MainBodyProps {
 }
 
 export default function MainBody({ category }: MainBodyProps) {
+  const filterList = itemListDummy.filter(
+    item => item.category === category.toLowerCase()
+  )
+
   function handleClickPageUp() {
     const scrollY = window.scrollY
     if (scrollY > 0) {
@@ -22,27 +26,28 @@ export default function MainBody({ category }: MainBodyProps) {
 
   return (
     <div className={styles.mainBodyBox}>
-      <div className={styles.contentBox}>
-        {itemListDummy.map(item => {
+      <ul className={styles.contentBox}>
+        {filterList.map(item => {
           return (
-            <MainReviewItem
-              key={item.id}
-              title={item.title}
-              category={item.category}
-              content={item.content}
-              like={item.liked}
-              userId={item.review_owner_id}
-            />
+            <li key={item.id}>
+              <MainReviewItem
+                title={item.title}
+                category={item.category}
+                content={item.content}
+                like={item.liked}
+                userId={item.review_owner_id}
+              />
+            </li>
           )
         })}
-      </div>
+      </ul>
       <button
         type="button"
         onClick={handleClickPageUp}
         className={styles.pageUpButton}
-        title="페이지 상단으로 이동"
+        aria-label="페이지 상단으로 이동"
       >
-        <ArrowBigUp width={25} height={25} />
+        <ArrowBigUp width={25} height={25} aria-hidden="true" />
       </button>
     </div>
   )
