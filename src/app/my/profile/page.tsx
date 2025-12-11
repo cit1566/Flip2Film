0241a18a06b3378a281211ebcc2ae5b3c53685a3
Heosion <tldhs123e@gmail.com>
2025-12-11 14:31:26 +0900

    feat : 프로필 수정 페이지 디자인 제작 - 프로필 업로드 재사용성이 있어 atom 폴더 안에 이동 - 공통 input css 안에 disabled인 경우, color 색 지정

---
+++ src/app/my/profile/page.tsx
@@ -0,0 +1,9 @@
+import MyProfileForm from "@/components/my-profile/my-profile-form/my-profile-form"
+
+export default function MyProfilePage() {
+  return (
+    <section>
+      <MyProfileForm />
+    </section>
+  )
+}
