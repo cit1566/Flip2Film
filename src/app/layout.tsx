a710a9fbfea1d383ce9fcfa3e8d9e90f290327c0
cit1566 <cit1566@gmail.com>
2025-12-16 15:30:59 +0900

    feat : reat-query Provider 컴포넌트 추가 - layout 컴포넌트 Provider로 감싸기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { PropsWithChildren } from "react"
 import Header from "../components/header/header"
 import PageTransition from "../components/page-transition/page-transition"
 import styles from "./layout.module.css"
+import Providers from "./providers"
 
 export const metadata: Metadata = {
   title: "Next.js 프로젝트 템플릿",
@@ -21,13 +22,18 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko-KR">
       <body className={pretendard.className}>
-        {/* 헤더 컴포넌트 */}
-        <Header className={styles.header}></Header>
-        {/* 메인 page.tsx */}
-        <PageTransition className={styles.container}>{children}</PageTransition>
-        <footer className={styles.footer}>
-          © Flip2Film All rights reserved. @GitHub
-        </footer>
+        <Providers>
+          {/* 헤더 컴포넌트 */}
+          <Header className={styles.header}></Header>
+          {/* 메인 page.tsx */}
+          <PageTransition className={styles.container}>
+            {children}
+          </PageTransition>
+          <footer className={styles.footer}>
+            © Flip2Film All rights reserved. @GitHub
+          </footer>
+          {children}
+        </Providers>
       </body>
     </html>
   )
