aac93c12dae81ed2db3b786ae1e8d34c5b705208
cit1566 <cit1566@gmailc.om>
2025-12-30 14:59:24 +0900

    refactor : util 함수 - chunkArray 에러 상황 추가 - 배열 길이 0 일경우 에러 반환

--- src/utils/chunkArray.ts
+++ src/utils/chunkArray.ts
@@ -1,5 +1,9 @@
 // 유틸리티 함수: 배열을 N개씩 묶기
 const chunkArray = <T>(array: T[], size: number): T[][] => {
+  if (size <= 0) {
+    throw new Error("Chunk size must be greater than 0")
+  }
+
   const result: T[][] = []
   for (let i = 0; i < array.length; i += size) {
     result.push(array.slice(i, i + size))
