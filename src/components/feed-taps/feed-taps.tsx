95ad1f9168221803a233c01ef5f423aafc00c124
cit1566 <cit1566@gmail.com>
2025-12-31 14:24:16 +0900

    refactor : feed-taps 스타일 리펙토링

--- src/components/feed-taps/feed-taps.tsx
+++ src/components/feed-taps/feed-taps.tsx
@@ -7,70 +7,68 @@ import DUMMYData from "./feed.json"
 import NoReviews from "./no-reviews"
 
 // -----------------------------------------------------------
-// 카드 아이템 타입
+// 타입
 export interface CardItem {
   id: number
   title: string
   description: string
 }
 
-// 탭 타입
 export interface Tab {
   id: string
   label: string
   items: CardItem[]
 }
 
-// 전체 데이터 타입
 export type TabsData = Tab[]
 
 // -----------------------------------------------------------
 
 export default function FeedTaps() {
   const [selectedTab, setSelectedTab] = useState(0)
-
   const tabs: TabsData = DUMMYData
 
   return (
     <section className={styles.feedTapsSection}>
       <h2 className="sr-only">사용자 감상평 피드</h2>
+
       <div className={styles.tabsContainer}>
+        {/* Tabs */}
         <div className={styles.tabs} role="tablist">
           {tabs.map((tab, index) => (
             <button
-              className={
-                selectedTab === index
-                  ? `${styles.tab} ${styles.isActive}`
-                  : styles.tab
-              }
               key={tab.id}
               role="tab"
               id={tab.id}
               aria-selected={selectedTab === index}
               aria-controls={`${tab.id}-panel`}
               tabIndex={selectedTab === index ? 0 : -1}
+              className={`${styles.tab} ${
+                selectedTab === index ? styles.isActive : ""
+              }`}
               onClick={() => setSelectedTab(index)}
             >
               {tab.label}
             </button>
           ))}
         </div>
+
+        {/* Panels */}
         {tabs.map((tab, index) => (
           <div
-            className={styles.tabPanel}
             key={`${tab.id}-panel`}
             role="tabpanel"
             id={`${tab.id}-panel`}
             aria-labelledby={tab.id}
-            hidden={selectedTab !== index ? true : undefined}
-            tabIndex={0}
+            hidden={selectedTab !== index}
+            className={styles.tabPanel}
           >
             {tab.items.length === 0 ? (
               <NoReviews />
             ) : (
               <ul className={styles.reviewList}>
                 {tab.items.map(item => (
-                  <li className={styles.listItem} key={item.id}>
+                  <li key={item.id} className={styles.listItem}>
                     <FeedCard {...item} />
                   </li>
                 ))}
