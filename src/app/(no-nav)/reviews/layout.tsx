12f029cb513f9ce050f191b8b999412896ffe5e3
cit1566 <cit1566@gmailc.om>
2026-01-04 12:26:42 +0900

    feat : 작성페이지 해더 추가

--- src/app/(no-nav)/reviews/layout.tsx
+++ src/app/(no-nav)/reviews/layout.tsx
@@ -10,6 +10,15 @@ export default function NoNavLayout({ children }: PropsWithChildren) {
 
       {/* 메인 page.tsx */}
       <div className={styles.mainBody}>
+        <div className={styles.reviewHeader}>
+          <div className={styles.reviewHeaderInner}>
+            새글 작성
+            <div className={styles.reviewHeaderButtonBox}>
+              <button type="button">저장</button>
+              <button type="button">발행</button>
+            </div>
+          </div>
+        </div>
         <div className={styles.container}>
           <PageTransition>{children}</PageTransition>
         </div>
