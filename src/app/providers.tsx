127afa1df260947ec973377c44fc43fad037b48b
cit1566 <cit1566@gmailc.om>
2026-01-06 00:59:35 +0900

    ci : tanstack query devtools install - get-query-client : using tanstack query docs with refactor

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,16 @@
 "use client"
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import React, { useState } from "react"
+import { getQueryClient } from "@/app/get-query-client"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import type * as React from "react"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // 렌더마다 새로 만들면 캐시가 날아가니  useState로 1회 생성
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 1,
-            staleTime: 1000 * 30,
-          },
-        },
-      })
-  )
+  const queryClient = getQueryClient()
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
